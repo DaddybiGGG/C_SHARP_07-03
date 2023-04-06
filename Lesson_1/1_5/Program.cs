e32284f919f099c0b2ec45807be03aafa2ada851
Svyatoslav <shapovalovmvp@gmail.com>
2023-04-06 15:13:20 +0300

    hw 1_4

--- Lesson_1/1_5/Program.cs
+++ Lesson_1/1_5/Program.cs
@@ -1,19 +1,19 @@
-﻿
-Console.Write("N=");
+﻿Console.Write("N=");
 int n = int.Parse(Console.ReadLine()!);
 
 int count = -n;
 
 while (count != n)
 {
-Console.WriteLine(count);
-if (n < 0)
-{
-count--;
-}
-else
-{
-count++;
-}
+    Console.WriteLine(count);
+    if (n < 0)
+    {
+        count--;
+    }
+    else
+    {
+        count++;
+    }
 
 }
+
---
+++ Lesson_1/HW/1_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int i = 2;
+
+if (n > 1)
+{
+    while (i <= n)
+    {
+        Console.Write(i + " ");
+        i = i + 2;
+        
+    }
+}
