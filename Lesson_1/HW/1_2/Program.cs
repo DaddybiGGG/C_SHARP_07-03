c83d8d47cbae6726cde088cbb064f2a35825b7e3
Svyatoslav <shapovalovmvp@gmail.com>
2023-04-06 14:36:50 +0300

    hw 1_3

---
+++ Lesson_1/HW/1_2/Program.cs
@@ -0,0 +1,25 @@
+﻿int max = 0;
+
+Console.Write("Введите 1 число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите 2 число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите 3 число: ");
+int num3 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    max = num1;
+}
+else if (num2 > num3)
+{
+    max = num2;
+}
+else if (num3 > num1)
+{
+    max = num3;
+}
+
+Console.WriteLine("max = " + max);
---
+++ Lesson_1/HW/1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1 = 0;
+
+Console.Write("Please enter number = ");
+num1 = int.Parse(Console.ReadLine()!);
+
+if ((num1 % 2) == 0)
+{
+    Console.WriteLine("yes!");
+}
+else
+{
+    Console.WriteLine("no!");
+}
