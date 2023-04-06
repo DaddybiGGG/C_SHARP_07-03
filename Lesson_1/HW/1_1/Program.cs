3811c31be6e4d69006a99269ece493a8b952be63
Svyatoslav <shapovalovmvp@gmail.com>
2023-04-06 14:12:05 +0300

    hw 1_1

---
+++ Lesson_1/HW/1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿int max = 0;
+int min = 0;
+
+Console.Write("Введите 1 число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите 2 число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    max = num1;
+    min = num2;
+}
+else
+{
+    max = num2;
+    min = num1;
+}
+
+Console.WriteLine("max = " + max + " и " + "min = " + min);
+
+
