204db83cf0de649712b32d144550ea3f5f6a3aa5
Svyatoslav <shapovalovmvp@gmail.com>
2023-04-06 12:06:53 +0300

    task 1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int num1 = 0;
+int num2 = 0;
+
+Console.Write("Please enter number 1 = ");
+num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Please enter number 2 = ");
+num2 = int.Parse(Console.ReadLine()!);
+
+if ((num2 * num2) == num1)
+{
+    Console.WriteLine("yes!");
+}
+else
+{
+    Console.WriteLine("no!");
+}
