a11f5842cb0f4605db4bff95258ff9f2e42136d8
Svyatoslav <shapovalovmvp@gmail.com>
2023-04-07 13:56:08 +0300

    hw 2_1

---
+++ Lesson_2/HW/2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num / 10 % 10;
+}
+// 123 / 10 -> 12
+// 123 % 10 -> 3
+
+int th_num = new Random().Next(100, 1000);
+int result = TakeNum(th_num);
+Console.WriteLine(result);
