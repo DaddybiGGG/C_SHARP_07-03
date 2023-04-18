561852e40aa675dd3bb05fb511f005d20d010bca
Svyatoslav <shapovalovmvp@gmail.com>
2023-04-18 20:42:18 +0300

    dota 2-18.04

--- Lesson_5/5_2/Program.cs
+++ Lesson_5/5_2/Program.cs
@@ -3,39 +3,44 @@
 
 
 Console.Write("Кол-во элементов:");
-int count =int.Parse(Console.ReadLine()!);
+int count = int.Parse(Console.ReadLine()!);
 Console.Write("Начало диапозона:");
-int a =int.Parse(Console.ReadLine()!);
+int a = int.Parse(Console.ReadLine()!);
 Console.Write("Конец диапозона:");
-int b =int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
 
-int[] MakeArray(int c, int m, int n){
-int[] array =new int[c];
+int[] MakeArray(int c, int m, int n)
+{
+    int[] array = new int[c];
 
-for (int i=0;i<array.Length;i++) array[i]=new Random().Next(m,n+1);
-return array;
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(m, n + 1);
+    return array;
 }
 
-void PrintArray(int[] arr){
-for (int i=0;i<arr.Length;i++) Console.Write($"{arr[i]} ");
-Console.WriteLine();
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++) Console.Write($"{arr[i]} ");
+    Console.WriteLine();
 }
 
-int[] array=MakeArray(count, a,b);
+int[] array = MakeArray(count, a, b);
 PrintArray(array);
 ////////////////////////////////////////////
 
-bool FindNumber (int[] arr, int n){
+bool FindNumber(int[] arr, int n)
+{
 
-for (int i=0;i<arr.Length;i++){
-if (arr[i]==n){
-return true;
-}
-}
-return false;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == n)
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
 Console.Write("Введите число:");
-int n= int.Parse(Console.ReadLine()!);
+int n = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine(FindNumber(array,n));
+Console.WriteLine(FindNumber(array, n));
---
+++ Lesson_5/HW/5_1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+int[] MakeArray(int length, int min, int max)
+{
+int[] arr = new int[length];
+for(int i = 0; i < length; i++)
+{
+arr[i] = new Random().Next(min, max + 1);
+}
+return arr;
+}
+
+Console.WriteLine(MakeArray(arr));
